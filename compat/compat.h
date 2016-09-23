ec8adc8df7b589f3723738eaa311fc578d423f65
Tim Kuijsten <tim@netsend.nl>
2016-09-23 12:43:39 +0200

    only define strlcat and strlcpy on linux

--- compat/compat.h
+++ compat/compat.h
@@ -1,8 +1,10 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
+#ifdef __linux__
 size_t strlcpy(char *dst, const char *src, size_t size);
 size_t strlcat(char *dst, const char *src, size_t size);
+#endif
 
 #endif
 
