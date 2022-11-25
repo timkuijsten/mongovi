7cac140da451b4ac68ea853ad67798610645c23e
Tim Kuijsten <info+git@netsend.nl>
2022-11-25 23:30:51 +0100

    fix compilation on macOS
    Always define strlcpy, not only on Linux.

--- compat/compat.h
+++ compat/compat.h
@@ -4,9 +4,7 @@
 #include <sys/types.h>
 #include <histedit.h>
 
-#ifdef __linux__
 size_t strlcpy(char *dst, const char *src, size_t size);
-#endif
 
 void *reallocarray(void *optr, size_t nmemb, size_t size);
 int backup_el_source(EditLine *el);
